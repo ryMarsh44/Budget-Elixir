14924698867edb519163be6460f5eed4dd140856
Ryan Marsh <ryan.marsh44@gmail.com>
2019-03-10 19:29:42 -0600

    adds tests for add, remove, and list categories
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- lib/budget/category.ex
+++ lib/budget/category.ex
@@ -1,6 +1,8 @@
 defmodule Budget.Category do
   use Ecto.Schema
+  alias Budget.Category
   alias Budget.Expenditure
+  alias Budget.Repo
 
   schema "categories" do
     has_many :expenditures, Expenditure
@@ -8,4 +10,34 @@ defmodule Budget.Category do
     timestamps()
   end
 
+
+end
+
+defmodule Budget.CategoryService do
+
+  import Ecto.Query
+  alias Budget.Repo
+  alias Budget.Category
+
+  def add(nil), do: nil
+
+  def add([]), do: nil
+
+  @spec add(List.t) :: [%Category{}]
+  def add([head | tail]) do
+    [add(head)] ++ add(tail)
+  end
+
+  @spec add(String.t) :: %Category{}
+  def add(name) do
+    %Category{name: name} |> Repo.insert
+  end
+
+  def remove(name) do
+    Repo.all(from c in Category, where: c.name == ^name, select: c)
+  end
+
+  def list() do
+    Enum.map Repo.all(from c in Category, select: c), &(&1.name)
+  end
 end
--- test/category.ex
+++ test/category.ex
@@ -3,6 +3,8 @@ defmodule Expenditure do
 
   alias Budget.Repo
   alias Budget.Expenditure
+  alias Budget.Category
+  alias Budget.CategoryService
 
   setup_all do
     {:ok, pid} = Budget.start(nil, nil)
@@ -17,22 +19,52 @@ defmodule Expenditure do
   end
 
   test "can add categories to budget successfully" do
+    first_category = "One"
+    {:ok, category} = CategoryService.add(first_category)
+    assert category.name == first_category
 
+
+    category_list = ["two", "three", "four"]
+    [{:ok, second} | _] = CategoryService.add(category_list)
+    assert second.name == "two"
   end
 
   test "can add duplicate category to budget - idempotent" do
+    duplicate = "duplicate"
+    {:ok, category} = CategoryService.add(duplicate)
+    {:ok, category} = CategoryService.add(duplicate)
 
   end
 
   test "can remove category with no associated expenditures" do
+    remove = "remove"
+    {:ok, remove_category} = CategoryService.add(remove)
 
+    [test_removed | _ ] = CategoryService.remove(remove)
+    assert test_removed.name == remove
+
+  end
+
+  test "can see all categories" do
+    Repo.delete_all(Category)
+    assert [] == CategoryService.list()
+
+    category_list = ["two", "three", "four"]
+    CategoryService.add(category_list)
+
+    assert category_list == CategoryService.list()
+
+    [remove_two | test_after_remove] = category_list
+    CategoryService.remove(remove_two)
+
+    assert test_after_remove = CategoryService.list()
   end
 
   test "can remove category with associated expenditures causing expenditures to be assigned to general category" do
 
   end
 
-  test "returns error when deleting non-existent category" do
-
+  test "returns empty list when deleting non-existent category" do
+    assert [] == CategoryService.remove("NOT HERE")
   end
 end
